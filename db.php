86f7d0e885f56b07146e0074b44b5bd08a0b2fa0
KabriAcid <kabriacid01@gmail.com>
2025-07-09 11:07:41 +0100

    Renamed the database

--- db.php
+++ db.php
@@ -1,4 +1,4 @@
 <?php
-$pdo = new PDO('mysql:host=localhost;dbname=test', 'root', '', [
+$pdo = new PDO('mysql:host=localhost;dbname=tracker', 'root', '', [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
 ]);
